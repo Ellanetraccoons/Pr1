81b28dda3ce7813aa83f3a7332d8afcdd6b92cdb
Ellanetraccoons <119304682+Ellanetraccoons@users.noreply.github.com>
2023-03-14 14:12:12 +0300

    Add files via upload

---
+++ ConsoleApplication17.cpp
@@ -0,0 +1,22 @@
+
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	int mas[7];
+
+	for (int i = 0; i < 7; i++)
+	{
+		scanf_s("%i", &mas[i]);
+	}
+	for (int i = 0; i < 7; i++)
+	{
+		printf("%i  ", mas[i]);
+	}
+
+	system("pause");
+}
+
