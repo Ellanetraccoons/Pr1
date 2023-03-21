83b79f3e088b8adfd40e327dde2687f408f11e4f
Ellanetraccoons <119304682+Ellanetraccoons@users.noreply.github.com>
2023-03-21 15:22:29 +0300

    Update ConsoleApplication18.cpp

--- ConsoleApplication18.cpp
+++ ConsoleApplication18.cpp
@@ -8,11 +8,11 @@ int main()
 	int n;
 	int mas[100];
 	printf("kol el");
-	scanf_s("%i",&n);
+	scanf("%i",&n);
 
 	for (int i = 0; i < n; i++)
 	{
-		scanf_s("%i", &mas[i]);
+		scanf("%i", &mas[i]);
 	}
 	for (int i = 0; i < n; i++)
 	{
